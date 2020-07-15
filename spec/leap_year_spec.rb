98f0f5fb8cf06627b636d146fba9bd0eda4a9372
Rich Ewin <richardewinlambert@gmail.com>
2020-07-15 10:33:11 +0100

    RED returns true if year is divisible by 400

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -8,4 +8,9 @@ describe LeapYear do
   it 'receives a year and confirms if it is or isnt a leap year' do
     expect(subject).to respond_to(:year).with(1).arguments
   end
+
+  # if year is % by 400 return true
+  it 'returns true if year is divisible by 400' do
+    expect(subject.year(2000)).to eq true
+  end
 end
