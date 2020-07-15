f92ad41ab97f6e6a9adacdfb59c227053c2ead10
Rich Ewin <richardewinlambert@gmail.com>
2020-07-15 10:37:50 +0100

    RED returns false if year is divisible by 100 but not 400

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -13,4 +13,9 @@ describe LeapYear do
   it 'returns true if year is divisible by 400' do
     expect(subject.year(2000)).to eq true
   end
-end
+
+  # if year is % by 100 but not 400 return false
+  it 'returns false if year is divisible by 100 but not 400' do
+    expect(subject.year(1700)).to eq false
+  end
+end 
